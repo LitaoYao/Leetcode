b096021439380667cf992767bd82803784da63dd
bobyao@foxmail.com <ylt@BobYaos-MacBook-Pro.local>
2021-05-03 20:10:53 +0800

    rename the content problems

---
+++ 11.cpp
@@ -0,0 +1,45 @@
+// https://leetcode-cn.com/problems/container-with-most-water/
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int calcArea(const vector<int>& height, int l, int r)
+	{
+		return (r - l) * min(height[l], height[r]);
+	}
+
+	int maxArea(vector<int>& height) {
+		int left = 0;
+		int right = height.size() - 1;
+		int ans = 0;
+		while(left <= right)
+		{
+			ans = max(ans, calcArea(height, left, right));
+			if(height[left] < height[right])
+			{
+				++left;
+			}
+			else if(height[left] > height[right])
+			{
+				--right;
+			}
+			else
+			{
+				++left;
+				--right;
+			}
+		}
+		return ans;
+	}
+};
+
+int main()
+{
+	vector<int> height = {2,3,4,5,18,17,6};
+	cout << Solution().maxArea(height) << endl;
+	return 0;
+}
