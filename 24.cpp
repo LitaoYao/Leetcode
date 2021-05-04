dbfc43a7edcca8ecd80753937af62f4ebbae87e4
yaobob@foxmail.com <ylt@BobYaos-MacBook-Pro.local>
2021-05-04 19:14:11 +0800

    one more problem

---
+++ 24.cpp
@@ -0,0 +1,49 @@
+// https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode() : val(0), next(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+		ListNode* pre = nullptr;
+		ListNode* slow = head;
+		ListNode* fast = nullptr;
+		if(slow != nullptr)
+		{
+			fast = slow->next;
+		}
+		while(slow != nullptr && fast != nullptr)
+		{
+			if(slow == head)
+			{
+				head = fast;
+			}
+			if(pre != nullptr)
+			{
+				pre->next = fast;
+				slow->next = fast->next;
+				fast->next = slow;
+			}
+			else
+			{
+				slow->next = fast->next;
+				fast->next = slow;
+			}
+			pre = slow;
+			slow = slow->next;
+			if(slow != nullptr)
+			{
+				fast = slow->next;
+			}
+		}
+		return head;
+    }
+};
