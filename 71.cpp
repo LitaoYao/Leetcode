e88b8d12210a623076a76954f2af4d9f1f7d7aad
yaobob@foxmail.com <ylt@BobYaos-MacBook-Pro.local>
2021-05-09 19:43:10 +0800

    2 grid problems

---
+++ 71.cpp
@@ -0,0 +1,65 @@
+// https://leetcode-cn.com/problems/simplify-path/
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	void stack_in(vector<string>& dirs, string dir)
+	{
+		dirs.push_back(dir);
+	}
+	void stack_out(vector<string>& dirs)
+	{
+		if(dirs.size())
+		{
+			dirs.pop_back();
+		}
+	}
+	void deal_dir(vector<string>& dirs, string dir)
+	{
+		if(dir == "..")
+		{
+			stack_out(dirs);
+		}
+		else if(dir != "." && dir != "")
+		{
+			stack_in(dirs, dir);
+		}
+	}
+	string simplifyPath(string path) {
+		vector<string> dirs;
+		string dir = "";
+		for(int i = 0; path[i] != '\0'; ++i)
+		{
+			if(path[i] == '/')
+			{
+				deal_dir(dirs, dir);
+				dir = "";
+			}
+			else
+			{
+				dir += path[i];
+			}
+		}
+		deal_dir(dirs, dir);
+		int dir_size = dirs.size();
+		string ans = "";
+		for(int i = 0; i < dir_size; ++i)
+		{
+			ans += "/" + dirs[i];
+		}
+		if(ans == "")
+		{
+			ans = "/";
+		}
+		return ans;
+	}
+};
+
+int main()
+{
+	cout << Solution().simplifyPath("/a/./b/../../c/");
+	return 0;
+}
---
+++ 73.cpp
@@ -0,0 +1,92 @@
+// https://leetcode-cn.com/problems/set-matrix-zeroes/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	void setZeroes(vector<vector<int>>& matrix) {
+		int n = matrix.size();
+		int m = matrix[0].size();
+		bool row0 = false;
+		for(int i = 0; i < n; ++i)
+		{
+			if(matrix[i][0] == 0)
+			{
+				row0 = true;
+				break;
+			}
+		}
+		bool col0 = false;
+		for(int j = 0; j < m; ++j)
+		{
+			if(matrix[0][j] == 0)
+			{
+				col0 = true;
+				break;
+			}
+		}
+		for(int i = 1; i < n; ++i)
+		{
+			for(int j = 1; j < m; ++j)
+			{
+				if(matrix[i][j] == 0)
+				{
+					matrix[0][j] = matrix[i][0] = 0;
+				}
+			}
+		}
+		for(int i = 1; i < n; ++i)
+		{
+			if(matrix[i][0] == 0)
+			{
+				for(int j = 1; j < m; ++j)
+				{
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		for(int j = 1; j < m; ++j)
+		{
+			if(matrix[0][j] == 0)
+			{
+				for(int i = 1; i < n; ++i)
+				{
+					matrix[i][j] = 0;
+				}
+			}
+		}
+		if(row0)
+		{
+			for(int i = 0; i < n; ++i)
+			{
+				matrix[i][0] = 0;
+			}
+		}
+		if(col0)
+		{
+			for(int j = 0; j < m; ++j)
+			{
+				matrix[0][j] = 0;
+			}
+		}
+		for(int i = 0; i < n; ++i)
+		{
+			for(int j = 0; j < m; ++j)
+			{
+				cout << matrix[i][j] << " ";
+			}
+			cout << endl;
+		}
+	}
+};
+
+int main()
+{
+	vector<vector<int>> matrix;
+	matrix.push_back({1, 1, 1});
+	matrix.push_back({1, 0, 1});
+	matrix.push_back({1, 1, 1});
+	Solution().setZeroes(matrix);
+	return 0;
+}
