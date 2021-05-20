a8038c0cb66ef870051db09c03ff10dc9c38462b
yaobob@foxmail.com <ylt@BobYaos-MacBook-Pro.local>
2021-05-20 20:17:10 +0800

    try commit with right username

---
+++ 120.cpp
@@ -0,0 +1,37 @@
+// https://leetcode-cn.com/problems/triangle/
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	int minimumTotal(vector<vector<int>>& triangle) {
+		int n = triangle.size();
+		vector<int> dp(n);
+		dp[n - 1] = triangle[0][0];
+		for(int i = 1; i < n; ++i)
+		{
+			dp[n - i - 1] = dp[n - i] + triangle[i][0];
+			for(int j = 1; j < i; ++j)
+			{
+				dp[n - i + j - 1] = min(dp[n - i + j - 1], dp[n - i + j]) + triangle[i][j];
+			}
+			dp[n - 1] = dp[n - 1] + triangle[i][i];
+		}
+		int ans = dp[0];
+		for(int i = 1; i < n; ++i)
+		{
+			ans = min(ans, dp[i]);
+		}
+		return ans;
+	}
+};
+int main()
+{
+	vector<vector<int>> triangle;
+	triangle.push_back({2});
+	triangle.push_back({3, 4});
+	triangle.push_back({6, 5, 7});
+	triangle.push_back({4, 1, 8, 3});
+	cout << Solution().minimumTotal(triangle);
+	return 0;
+}
