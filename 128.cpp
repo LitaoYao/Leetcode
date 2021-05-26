04a8763813475aa2a85ea78034b801f8422b3ebc
LitaoYao <yaobob@foxmail.com>
2021-05-26 08:24:09 +0800

    three solutions

--- 128.cpp
+++ 128.cpp
@@ -8,6 +8,7 @@ using namespace std;
 class Solution {
 public:
 
+	// 方法一
     int longestConsecutive(vector<int>& nums) {
 		unordered_map<int, int> counts;
 		int max_count = 0;
@@ -34,12 +35,19 @@ public:
 				max_count = cur_count;
 			}
 			counts[num] = cur_count;
-			counts[num - left] = cur_count;
-			counts[num + right] = cur_count;
+			if(left)
+			{
+				counts[num - left] = cur_count;
+			}
+			if(right)
+			{
+				counts[num + right] = cur_count;
+			}
 		}
 		return max_count;
     }
 
+	// 方法二
     int longestConsecutive2(vector<int>& nums) {
 		unordered_set<int> num_set;
 		for(int num : nums)
@@ -51,11 +59,11 @@ public:
 		int now_count = 0;
 		for(const int& num : num_set)
 		{
-			if(num_set.find(num - 1) == num_set.end())
+			if(!num_set.count(num - 1))
 			{
 				now_num = num;
 				now_count = 1;
-				while(num_set.find(now_num + 1) != num_set.end())
+				while(num_set.count(now_num + 1))
 				{
 					++now_num;
 					++now_count;
@@ -65,11 +73,32 @@ public:
 		}
 		return max_count;
     }
+
+	// 方法三 并查集
+	int find_fa(int num, unordered_map<int, int>& fa)
+	{
+		return fa.count(num) ? fa[num] = find_fa(fa[num], fa) : num;
+	}
+
+    int longestConsecutive3(vector<int>& nums) {
+		unordered_map<int, int> fa;
+		for(int num : nums)
+		{
+			fa[num] = num + 1;
+		}
+        int ans = 0;
+		for(int num : nums)
+		{
+            int f = find_fa(num + 1, fa);
+            ans = max(ans, f - num);
+		}
+		return ans;
+    }
 };
 
 int main()
 {
-	vector<int> nums = {0,3,7,2,5,8,4,6,0,1};
-	cout << Solution().longestConsecutive(nums);
+	vector<int> nums = {0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
+	cout << Solution().longestConsecutive3(nums);
 	return 0;
 }
